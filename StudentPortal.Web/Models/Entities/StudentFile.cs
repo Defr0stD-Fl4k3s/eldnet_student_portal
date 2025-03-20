db48226e42e78a29242a756c207f76ae809efdbd
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-03-20 22:41:04 +0800

    Adding Entites folder then StudentFile.cs  within it

---
+++ StudentPortal.Web/Models/Entities/StudentFile.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class StudentFile
+    {
+        
+        public long STFSTUDID { get; set; }
+        
+        public string STFSTUDLNAME { get; set; }
+
+        public string STFSTUDFNAME { get; set; }
+
+        public string STFSTUDMNAME { get; set; }
+        
+        public string STFSTUDCOURSE { get;set; }
+
+        public string STFSTUDYEAR { get;set; }
+
+        public string STFSTUDREMARKS{ get; set; }
+
+        public string STFSTUDSTATUS { get; set; }
+    }
+}
