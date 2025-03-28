9e24fd7c7777abc97cfdc8c0115c62b31c9ba996
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-03-29 01:55:58 +0800

    Adding the AddSubjects portions

---
+++ StudentPortal.Web/Controllers/SubjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddSubjects()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentPortal.Web/Models/Entities/SubjectFile.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class SubjectFile
+    {
+        public string SFSUBJCODE { get; set; }
+
+        public string SFSUBJDESC { get; set; }
+
+        public float SFSUBJUNITS { get; set; }
+
+        public int SFSUBJREGOFRNG { get; set; }
+
+        public string SFSUBJCATEGORY { get; set; }
+
+        public int SFSUBJSTATUS { get; set; }
+
+        public string SFSUBJCOURSECODE { get; set; }
+
+        public SFSUBJSTATUSEnum SFSUBJCURRCODE { get; set; }
+
+    }
+
+    public enum SFSUBJSTATUSEnum
+    {
+        AC,
+        IN
+    }
+}
