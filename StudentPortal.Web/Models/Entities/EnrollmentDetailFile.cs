6ac2534a18cc6cfdc05158fc2a030455160526a9
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-04-01 16:19:52 +0800

    adding the EnrollmentDetailFile and also attributes within it

---
+++ StudentPortal.Web/Models/Entities/EnrollmentDetailFile.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Models.Entities
+{
+    public class EnrollmentDetailFile
+    {
+        public long ENRDFSTUDID { get; set; } 
+
+        [StringLength(15)]
+        public string ENRDFSTUDSUBJCDE { get; set; } 
+
+        [StringLength(8)]
+        public string ENRDFSTUDEDPCODE { get; set; } /
+
+        public EnrollmentDetailStatus ENRDFSTUDSTATUS { get; set; } 
+    }
+    public enum EnrollmentDetailStatus
+    {
+        Withdrawn,
+        Cancelled
+    }
+}
