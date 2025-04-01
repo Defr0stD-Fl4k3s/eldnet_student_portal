c55dc0af1e13ccf01acb7b02b6214e8b384235be
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-04-01 16:18:02 +0800

    Adding attributes inside the EnrollmentHeaderFile

--- StudentPortal.Web/Models/Entities/EnrollmentHeaderFile.cs
+++ StudentPortal.Web/Models/Entities/EnrollmentHeaderFile.cs
@@ -1,6 +1,27 @@
-﻿namespace StudentPortal.Web.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Models.Entities
 {
     public class EnrollmentHeaderFile
     {
+        public long ENRHFSTUDID { get; set; }
+
+        public DateTime ENRHFSTUDDATEENROLL { get; set; } 
+
+        [StringLength(15)]
+        public string ENRHFSTUDSCHLYR { get; set; } 
+
+        [StringLength(15)]
+        public string ENRHFSTUDENCODER { get; set; }
+
+        public double ENRHFSTUDTOTALUNITS { get; set; } 
+
+        public EnrollmentStatus ENRHFSTUDSTATUS { get; set; } 
+    }
+
+    public enum EnrollmentStatus
+    {
+        EN, // Enrolled
+        UN  // Unenrolled
     }
 }
