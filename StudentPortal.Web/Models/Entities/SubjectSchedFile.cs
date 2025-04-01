ddf48b432a1f8492086a0c6d6a1866ffd2f72480
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-04-01 15:49:06 +0800

    adding SubjectSchedFile attributes in our code

--- StudentPortal.Web/Models/Entities/SubjectSchedFile.cs
+++ StudentPortal.Web/Models/Entities/SubjectSchedFile.cs
@@ -2,5 +2,25 @@
 {
     public class SubjectSchedFile
     {
+        public string SSFEDPCODE { get; set; }
+        public string SSFSUBJCODE { get; set; }
+        public DateTime SSFSTARTTIME   { get; set; }
+        public DateTime SSFENDTIME { get; private set; }
+        public string SSFDAYS { get; set; } 
+        public string SSFROOM {  get; set; } 
+
+        public int SSFMAXSIZE { get; set; }
+
+        public int SSFCLASSSIZE {  get; set; }
+
+        public string SSFSTATUS { get; set; }
+        
+        public string SSFXM { get; set; }
+
+        public string SSFSECTION { get; set; }
+
+        public int SSFSCHOOLYEAR { get; set; }
+
+
     }
 }
