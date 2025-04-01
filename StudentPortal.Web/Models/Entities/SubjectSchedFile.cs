b76fdcc588913c8ddfa246d132636e73d73a8831
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-04-01 16:02:48 +0800

    adding some few files of code for the SubjectSchedFile

--- StudentPortal.Web/Models/Entities/SubjectSchedFile.cs
+++ StudentPortal.Web/Models/Entities/SubjectSchedFile.cs
@@ -1,11 +1,16 @@
-﻿namespace StudentPortal.Web.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Models.Entities
 {
     public class SubjectSchedFile
     {
+        [StringLength(8)]
         public string SSFEDPCODE { get; set; }
+        [StringLength(15)]
         public string SSFSUBJCODE { get; set; }
         public DateTime SSFSTARTTIME   { get; set; }
         public DateTime SSFENDTIME { get; private set; }
+        [StringLength(3)]
         public string SSFDAYS { get; set; } 
         public string SSFROOM {  get; set; } 
 
@@ -13,14 +18,30 @@
 
         public int SSFCLASSSIZE {  get; set; }
 
-        public string SSFSTATUS { get; set; }
+        public SSFStatus SSFSTATUS { get; set; }
         
-        public string SSFXM { get; set; }
+        public SSFxm SSFXM { get; set; }
 
+        [StringLength(3)]
         public string SSFSECTION { get; set; }
 
         public int SSFSCHOOLYEAR { get; set; }
+            
 
+    }
 
+    public enum SSFStatus
+    {
+        Active,
+        Inactive,
+        Dissolved,
+        Restricted,
+        Closed
+    }
+
+    public enum SSFxm
+    {
+        AM,
+        PM
     }
 }
