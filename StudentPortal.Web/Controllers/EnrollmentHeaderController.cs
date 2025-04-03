ed45a3932d53e604027708af934eaa85cf84f0b2
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-04-04 00:43:54 +0800

    Adding Enrollment Header UI into the code

---
+++ StudentPortal.Web/Controllers/EnrollmentHeaderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class EnrollmentHeaderController : Controller
+    {
+        public IActionResult AddEnrollmentHeader()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentPortal.Web/Models/AddEnrollmentHeaderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Models
+{
+    [PrimaryKey(nameof(ENRHFSTUDID))]
+
+    public class AddEnrollmentHeaderViewModel
+    {
+        public long ENRHFSTUDID { get; set; }
+        public DateTime ENRHFSTUDDATEENROLL { get; set; }
+
+        [StringLength(15)]
+        public string ENRHFSTUDSCHLYR { get; set; }
+
+        [StringLength(15)]
+        public string ENRHFSTUDENCODER { get; set; }
+
+        public double ENRHFSTUDTOTALUNITS { get; set; }
+
+        [StringLength(2)]
+        [RegularExpression("EN|UN", ErrorMessage = "choose 2 Letter codes (e.g., EN or UN).")]
+
+        public string ENRHFSTUDSTATUS { get; set; }
+    }
+}
