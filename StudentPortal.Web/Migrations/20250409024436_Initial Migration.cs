11f1cf72584888091378b485a5f51b609bae1cba
Matthew James P. Fabon <matthewfabon11@gmail.com>
2025-04-09 11:55:40 +0800

    adding our studentportal into the SSMS

---
+++ StudentPortal.Web/Migrations/20250409024436_Initial Migration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPortal.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentFiles",
+                columns: table => new
+                {
+                    STFSTUDID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    STFSTUDLNAME = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    STFSTUDFNAME = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    STFSTUDMNAME = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    STFSTUDCOURSE = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    STFSTUDYEAR = table.Column<int>(type: "int", nullable: false),
+                    STFSTUDREMARKS = table.Column<int>(type: "int", maxLength: 15, nullable: false),
+                    STFSTUDSTATUS = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentFiles", x => x.STFSTUDID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentFiles");
+        }
+    }
+}
